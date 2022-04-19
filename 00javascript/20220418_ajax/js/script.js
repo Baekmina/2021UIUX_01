61a68eb0a799a99f734beaa44564089ee5bce389
백민아 <koyoa12@naver.com>
2022-04-19 14:04:41 +0900

    ajax v2

--- 00javascript/20220418_ajax/js/script.js
+++ 00javascript/20220418_ajax/js/script.js
@@ -22,17 +22,26 @@
 //     console.log(xhr,status,error)
 // }
 
-axios
-.get(
-    'https://dapi.kakao.com/v2/search/vclip',
-    {
-        params:{query:'BTS'},
-        headers:{Authorization:'KakaoAK 7f69c3e2fd32d6a14370c3ed9b0108a1'},
-    }
-)
-.then(onResult)
-.catch(onError);
 
+
+$(".search-form").submit(onSubmit);
+function onSubmit(e) {
+    e.preventDefault(); // 검색창에서 엔터 눌렀을 때 반짝하는 것을 막아줌
+    // document.querySelector("input[name='query']").value // 자바스크립트
+    let query = $(this).find("input[name='query']").val().trim();
+    // trim : 문자의 양 옆 공간을 없애주는 것
+
+    axios
+        .get(
+            'https://dapi.kakao.com/v2/search/web?size=50',
+            {
+                params: { query: query },
+                headers: { Authorization: 'KakaoAK 7f69c3e2fd32d6a14370c3ed9b0108a1' },
+            }
+        )
+        .then(onResult)
+        .catch(onError);
+}
 
 function onResult(v){
     console.log(v.data);
@@ -42,9 +51,9 @@ function onResult(v){
 
     // rDoc.forEach(function(item,index){});
     rDoc.forEach(function(item,index){
-        txt += `<div class="col-6 mb-3">
+        txt += `<div class="col-6 col-md-4 mb-3">
                     <h5><a href="${item.url}" target="_blank">${item.title}</a></h5>
-                    <p><img src="${item.thumbnail}" class="w-100"></p>
+                    <p>${item.contents}</p>
                 </div>`
     });
     
